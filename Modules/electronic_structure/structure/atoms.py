e7312654932413da3fb7c0cf1af1ea1238dbd1fa
alexanderbuccheri <alexanderbuccheri@googlemail.com>
2020-05-06 22:53:04 +0100

    Strong typing class of Atoms

--- Modules/electronic_structure/structure/atoms.py
+++ Modules/electronic_structure/structure/atoms.py
@@ -2,12 +2,26 @@
 
 import numpy as np
 from enum import Enum
+from typing import List
+
+from modules.parameters import elements
 
 class Atom:
-    def __init__(self, species, position):
+    def __init__(self, species: str, position: List[float]):
         self.species = species
-        #self.atomic_number = atomic_number
         self.position = np.asarray(position)
+    def atomic_number(self) -> int:
+        return elements.symbol_to_an[self.species]
+
+# Should this be a class or a function?
+class Atoms:
+    def __init__(self):
+        return
+    def __new__(self, species: List[str], positions: List[float]) -> List[Atom]:
+        molecule = []
+        for ia in range(0, len(species)):
+            molecule.append(Atom(species=species[ia], position=positions[ia]))
+        return molecule
 
 class CoordinateType(Enum):
     XYZ = 1
