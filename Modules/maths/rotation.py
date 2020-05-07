4dc5f0863bb1c7c31b864f8f54cf60f9cdf63856
alexanderbuccheri <alexanderbuccheri@googlemail.com>
2020-05-07 12:51:13 +0100

    Created name-appropriate files

---
+++ Modules/maths/rotation.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# TODO(Alex) Add asserts for when this won't work
+def rotation_to_align_a_with_b(a, b):
+    """
+    Generates a rotation matrix to align unit vector 'a' with unit vector 'b', 
+    such that 'a' and 'b' point in the same direction. 
+
+    Parameters
+    ----------
+    a : array_like
+        First vector.
+    b : array_like
+        Second vector.
+
+    Returns
+    -------
+    c : ndarray 
+        Rototation matrix .shape(3,3)
+
+    Notes
+    -----
+    Based on stack exchange:
+    https://math.stackexchange.com/questions/180418/
+    calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
+  
+    """
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    if not np.allclose(a, a/norm_a):
+        print('Input a vector not unit normal - normalising')
+        a = a / norm_a
+        print(a)
+    if not np.allclose(b, b/norm_b):
+        print('Input b vector not unit normal - normalising')
+        b = b / norm_b
+        print(b)
+
+    v = np.cross(a,b)
+    #s = np.linalg.norm(v)
+    c = np.dot(a,b)
+    f = 1./(1. + c)
+    vmat = np.array([[    0, -v[2],  v[1]],
+                     [ v[2],     0, -v[0]],
+                     [-v[1],  v[0],     0]])
+    return np.eye(3,3) + vmat + f *(np.matmul(vmat,vmat))
