c63cae45775f620641c577359dd460762e6d50e7
alexanderbuccheri <alexanderbuccheri@googlemail.com>
2020-05-19 20:00:47 +0100

    Generate command substrings in a completely generic way, given the data types I require

--- pycharm_projects/entos_total_energies/convergence_with_k_sampling.py
+++ pycharm_projects/entos_total_energies/convergence_with_k_sampling.py
@@ -11,6 +11,13 @@ class InputEntry:
         if unit == None:
             self.unit = ""
 
+class SingleOption:
+    def __init__(self, option):
+        self.option = option
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
 class EwaldOptions:
     def __init__(self, real_cutoff, recip_cutoff, alpha=None, relative_error=None):
         self.real_cutoff = InputEntry('ewald_real_cutoff', real_cutoff, "bohr")
@@ -20,30 +27,59 @@ class EwaldOptions:
         if relative_error != None:
             self.relative_error = InputEntry('ewald_relative_error', relative_error)
 
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+
 class TranslationCutoffOptions:
     def __init__(self, h0, overlap, repulsive, unit='bohr'):
         self.h0 = InputEntry('h0', h0, unit)
         self.overlap = InputEntry('overlap', overlap, unit)
         self.repulsive = InputEntry('repulsive', repulsive, unit)
+
     def __iter__(self):
         for attr, value in self.__dict__.items():
             yield attr, value
 
+
 class KGridOptions:
     def __init__(self, grid_integers, symmetry_reduction=False):
-        self.grid_integers = grid_integers
-        self.symmetry_reduction = symmetry_reduction
+        self.grid_integers = InputEntry("monkhorst_pack", grid_integers)
+        self.symmetry_reduction = InputEntry("symmetry_reduction", symmetry_reduction)
 
-def grid_integers_string(grid_integers):
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+
+def list_to_string(grid_integers):
     assert len(grid_integers) == 3
     return "[" + str(grid_integers[0]) + "," \
                + str(grid_integers[1]) + "," \
                + str(grid_integers[2]) + "]"
 
-def command_string(options):
+
+def generic_str(value) -> str:
+    if isinstance(value, str):
+        return value
+    elif isinstance(value, int):
+        return str(value)
+    elif isinstance(value, float):
+        return str(value)
+    elif isinstance(value, bool):
+        return str(value).lower()
+    elif isinstance(value, list):
+        return list_to_string(value)
+    else:
+        quit("Have not implemented type ", type(value), "in generic_str")
+
+
+def options_to_string(*args):
     string = ""
-    for key, entry in dict(options).items():
-        string += entry.command + " = " + str(entry.value) + " " + str(entry.unit) + '\n'
+    for options in args:
+        for key, entry in dict(options).items():
+            string += entry.command + " = " + generic_str(entry.value) + " " + str(entry.unit) + '\n'
     return string
 
 
@@ -58,59 +94,16 @@ def command_string(options):
 #                      )
 #         """
 #
-#     cutoff_options_string = \
-#         """    h0_cutoff = """ + str(cutoff_options.h0) + """ """ + cutoff_options.unit + """
-#         overlap_cutoff = """ + str(cutoff_options.overlap) + """ """ + cutoff_options.unit + """
-#         repulsive_cutoff = """ + str(cutoff_options.repulsive) + """ """ + cutoff_options.unit + """
-#     """
-#
-#     ewald_options_string = \
-#         """    ewald_real_cutoff = """ + str(ewald_options.real_cutoff) + """ bohr
-#         ewald_reciprocal_cutoff = """ + str(ewald_options.recip_cutoff) + """
-#         alpha = """ + str(ewald_options.alpha) + """
-#     """
-#
-#     k_grid_options_string = \
-#         """    monkhorst_pack = """ + grid_integers_string(mk_grid) + """
-#         symmetry_reduction  = """ + str(symmetry_reduction).lower() + """
-#     """
-#
-#     other_options_string = \
-#         """    temperature = """ + str(temperature) + """ kelvin
-#     """
-#
-#     entos_input_string = named_result + " := xtb(\n" + \
-#                          structure_string + \
-#                          cutoff_options_string + \
-#                          ewald_options_string + \
-#                          k_grid_options_string + \
-#                          other_options_string + "\n)"
-#     return entos_input_string
-#
 
-
-# Make these functions more modular
 
 named_result = "si_kgrid"
 cutoff_options = TranslationCutoffOptions(h0=40, overlap=40, repulsive=40)
-
-string = command_string(cutoff_options)
+ewald_options = EwaldOptions(real_cutoff=10, recip_cutoff=2, alpha=0.5)
+kgrid_options = KGridOptions(grid_integers=[2,2,2], symmetry_reduction=False)
+temperature_option = SingleOption(InputEntry(command="temperature", value=0, unit="kelvin"))
+string = options_to_string(cutoff_options, ewald_options, kgrid_options, temperature_option)
 print(string)
 
-ewald_options = EwaldOptions(real_cutoff=10, recip_cutoff=2, alpha=0.5)
-temperature = 0
-mk_grid = [2,2,2]
-kgrid_options = KGridOptions(grid_integers=mk_grid, symmetry_reduction=False)
-
-# Really want to make a list from the class, then iteate over to build "command = " + "value " + "unit"
-
-# entos string can just take **awgs
-# for every other string, define a string function to return it
-#Want to abstract this stuff so it's usable for each input
-
-# entos_input_string = generate_entos_command_string(named_result,
-#    cutoff_options, ewald_options, temperature, mk_grid, symmetry_reduction)
-#
 
 
 
