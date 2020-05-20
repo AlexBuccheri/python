c3a5423e9e6838a4e9f35de898255a43000ffc99
alexanderbuccheri <alexanderbuccheri@googlemail.com>
2020-05-20 13:15:17 +0100

    Add plotting

--- pycharm_projects/entos_total_energies/convergence_with_k_sampling.py
+++ pycharm_projects/entos_total_energies/convergence_with_k_sampling.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 import subprocess
 import matplotlib.pyplot as plt
+import time
 
 from options import *
 
@@ -24,15 +25,35 @@ def primitive_silicon(named_result, kgrid_options):
     return xtb_string
 
 
+def plot_convergence(n_k_points, n_irreducible_k_points, total_energy):
+    fig, ax = plt.subplots()
+    ax.plot(n_k_points, total_energy, color='blue', marker='o', linestyle='dashed', linewidth=2, markersize=12)
+
+    # Add number of irreducible k-points as numbers next to each point
+    for i, n_irreducible_k_point in enumerate(n_irreducible_k_points):
+        ax.annotate(n_irreducible_k_point, (n_k_points[i] + 0.02, total_energy[i] + 0.02), size=14, color='red')
+    #plt.legend([ax.annotate], ["Num of irreducible k-points"])
+
+    plt.title("Convergence with k-sampling. Bulk silicon", fontsize=16)
+    plt.xlabel("Number of Monkhorst-Pack k-points", fontsize=16)
+    plt.ylabel("Total Energy (H)", fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    return fig, ax
+
+
+
+
+
 # Run a job
 print_level = 1
 entos_exe = '/Users/alexanderbuccheri/Codes/entos/cmake-build-debug/entos'
-k_grids = [[1,1,1], [2,2,2], [3,3,3], [4,4,4], [5,5,5], [6,6,6], [7,7,7], [8,8,8]]
+k_grids = [[1,1,1], [1,2,1]]  #, [1,2,2], [2,2,2], [3,3,3], [4,4,4], [5,5,5], [6,6,6]]  #[7,7,7], [8,8,8]]
 
 # Converge total energy w.r.t. MP grid density
 n_k_points = []
 n_irreducible_k_points = []
 total_energy = []
+timing = []
 
 for k, k_grid in enumerate(k_grids):
     n_k_points.append(np.prod(k_grid))
@@ -41,7 +62,10 @@ for k, k_grid in enumerate(k_grids):
     silicon_input_string = primitive_silicon(named_result, kgrid_options)
 
     entos_command = [entos_exe, '--format', 'json', '-s', silicon_input_string]
+    t1 = time.perf_counter()
     entos_json_result = subprocess.check_output(entos_command)
+    t2 = time.perf_counter()
+    timing.append(t2-t1)
     result = json.loads(entos_json_result)
 
     if print_level > 0:
@@ -53,9 +77,9 @@ for k, k_grid in enumerate(k_grids):
 
 
 # Want to time it - debug verses release
-
-print("# N grid points, N irreducible grid points, total energy (H)")
+print("# N grid points, N irreducible grid points, total energy (H), time (s)")
 for k in range(0, len(k_grids)):
-    print(n_k_points[k], n_irreducible_k_points[k], total_energy[k])
+    print(n_k_points[k], n_irreducible_k_points[k], total_energy[k], timing[k])
 
-
+fig, ax = plot_convergence(n_k_points, n_irreducible_k_points, total_energy)
+plt.show()
