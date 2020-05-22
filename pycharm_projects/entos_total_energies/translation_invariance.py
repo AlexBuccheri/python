8e6798a5e3a8a41348143235939f4e7960fc0ee5
alexanderbuccheri <alexanderbuccheri@googlemail.com>
2020-05-22 17:46:13 +0100

    Added rutile and massively refactored the routine such that it can also generate entos input strings directly

--- pycharm_projects/entos_total_energies/translation_invariance.py
+++ pycharm_projects/entos_total_energies/translation_invariance.py
@@ -1,85 +1,122 @@
 # Translate the atoms in the primitive cell
 # by a fixed amount and confirm that the total energy is unchanged
+# Note, arbitrary shift should be small enough that all atoms stay within the unit cell
 
 import json
 import subprocess
 import collections
 
-def list_to_string(mylist: list, joiner: str) -> str:
-    return joiner.join([str(i) for i in mylist])
 
 entos_exe = '/Users/alexanderbuccheri/Codes/entos/cmake-build-debug/entos'
 
-def silicon_translational_invariance():
-    input_string = """bulk_si := xtb(
-    structure( fractional=[[Si,{pos0:s}],
-                           [Si,{pos1:s}]] 
-       lattice(a = 5.431 angstrom
-               bravais = fcc
-               )
-             )
-      h0_cutoff = 40 bohr
-      overlap_cutoff = 40 bohr
-      repulsive_cutoff = 40 bohr
-      ewald_real_cutoff = 10 bohr
-      ewald_reciprocal_cutoff = 2 
-      ewald_alpha = 0.5 
-      monkhorst_pack = [2,2,2] 
-      symmetry_reduction = true 
-      temperature = 0 kelvin
-    )
-    """
+# Run entos energy differences from python
+run_energy_differences = False
+
+# Generate equivalent app test inputs from python
+generate_app_test_inputs = True
+
+
+# -----------------------------------
+# Functions
+# -----------------------------------
+
+def list_to_string(mylist: list, joiner: str) -> str:
+    return joiner.join([str(i) for i in mylist])
+
+def get_energies(named_result, inputs):
+    energy = []
+    for input in inputs:
+        entos_command = [entos_exe, '--format', 'json', '-s', input.replace('\n', ' ')]
+        entos_output = subprocess.check_output(entos_command)
+        result = json.loads(entos_output)
+        energy.append(result[named_result]['energy'])
+    return energy
+
+def get_energy_differences(named_result, strings_function, arbitrary_shift):
+    input_strings = strings_function(named_result, arbitrary_shift)
+    energies = get_energies(named_result, input_strings)
+
+    energy_diffs = []
+    for i in range(1, len(energies)):
+        energy_diffs.append(abs(energies[i] - energies[i-1]))
+
+    return energy_diffs
+
+def evaluate_energy_differences(energy_difference, tol=1.e-6):
+    all_pass = True
+    for key, value in energy_difference.items():
+        if value > tol:
+            print(key + ' primitive unit cell')
+            print("Energy should be invariant w.r.t. translation of basis positions,"
+                  "however we find |delta E| (Ha) = ", value)
+            all_pass = False
+    return all_pass
+
+# ----------------------------------------
+# entos string functions for translations
+# ----------------------------------------
+
+def silicon_translational_invariance_strings(named_result, arbitrary_shift):
+    input_string = named_result + """ := xtb(
+        structure( fractional=[[Si,{pos0:s}],
+                               [Si,{pos1:s}]] 
+           lattice(a = 5.431 angstrom
+                   bravais = fcc
+                   )
+                 )
+          h0_cutoff = 40 bohr
+          overlap_cutoff = 40 bohr
+          repulsive_cutoff = 40 bohr
+          ewald_real_cutoff = 10 bohr
+          ewald_reciprocal_cutoff = 2 
+          ewald_alpha = 0.5 
+          monkhorst_pack = [2,2,2] 
+          symmetry_reduction = true 
+          temperature = 0 kelvin
+        )
+        """
 
     fractional_0 = [0.00, 0.00, 0.00]
     fractional_1 = [0.25, 0.25, 0.25]
 
-    arbitrary_shift = 0.43
     fractional_0_shifted = [round(i + arbitrary_shift, 3) for i in fractional_0]
     fractional_1_shifted = [round(i + arbitrary_shift, 3) for i in fractional_1]
 
     fractional_0_str = list_to_string(fractional_0, ',')
     fractional_1_str = list_to_string(fractional_1, ',')
-    input_noshift = input_string.format(pos0=fractional_0_str, pos1=fractional_1_str).replace('\n', ' ')
+    input_noshift = input_string.format(pos0=fractional_0_str, pos1=fractional_1_str)
 
     fractional_0_shifted_str = list_to_string(fractional_0_shifted, ',')
     fractional_1_shifted_str = list_to_string(fractional_1_shifted, ',')
-    input_shift = input_string.format(pos0=fractional_0_shifted_str, pos1=fractional_1_shifted_str).replace('\n', ' ')
-
-    energy = []
-    for input in [input_noshift, input_shift]:
-        entos_command = [entos_exe, '--format', 'json', '-s', input]
-        entos_output = subprocess.check_output(entos_command)
-        result = json.loads(entos_output)
-        energy.append(result['bulk_si']['energy'])
-
-    return abs(energy[1] - energy[0])
+    input_shift = input_string.format(pos0=fractional_0_shifted_str, pos1=fractional_1_shifted_str)
+    return (input_noshift, input_shift)
 
 
-def rutile_translational_invariance():
-    # Reference: https: // materialsproject.org / materials / mp - 2657 /
-    input_string = """bulk_rutile := xtb( 
-                    structure( fractional=[[Ti, {pos0:s}],
-                                           [Ti, {pos1:s}],
-                                           [ O, {pos2:s}],
-                                           [ O, {pos3:s}],
-                                           [ O, {pos4:s}],
-                                           [ O, {pos5:s}]]
-                           lattice( a = 4.6068 angstrom
-            	                    c = 2.9916 angstrom
-                                    bravais = tetragonal
-                                   )
-                             )
+def rutile_translational_invariance_strings(named_result, arb_shift):
+    # Reference: https://materialsproject.org/materials/mp-2657/
+    input_string = named_result + """ := xtb(
+                       structure( fractional=[[Ti, {pos0:s}],
+                                              [Ti, {pos1:s}],
+                                              [ O, {pos2:s}],
+                                              [ O, {pos3:s}],
+                                              [ O, {pos4:s}],
+                                              [ O, {pos5:s}]]
+                              lattice( a = 4.6068 angstrom
+               	                    c = 2.9916 angstrom
+                                       bravais = tetragonal
+                                      )
+                                )
 
-                     repulsive_cutoff = 40.0 bohr
-                     overlap_cutoff = 40.0 bohr
-                     h0_cutoff = 40.0 bohr
-                     temperature = 0 kelvin
-                     ewald_real_cutoff = 10 bohr
-                     ewald_reciprocal_cutoff = 2
-                     ewald_alpha = 0.5
-                     monkhorst_pack = [2, 2, 2]
-                     symmetry_reduction = false
-                    ) """
+                        repulsive_cutoff = 40.0 bohr
+                        overlap_cutoff = 40.0 bohr
+                        h0_cutoff = 40.0 bohr
+                        temperature = 0 kelvin
+                        ewald_real_cutoff = 10 bohr
+                        ewald_reciprocal_cutoff = 2
+                        ewald_alpha = 0.5
+                        monkhorst_pack = [2, 2, 2]
+                        symmetry_reduction = false
+                       ) """
 
     positions = [[0.000000, 0.000000, 0.000000],
                  [0.500000, 0.500000, 0.500000],
@@ -88,7 +125,46 @@ def rutile_translational_invariance():
                  [0.195526, 0.804474, 0.500000],
                  [0.804474, 0.195526, 0.500000]]
 
-    arb_shift = 0.134
+    fractional_strings = {}
+    fractional_shift_strings = {}
+    for i, position in enumerate(positions):
+        fractional_strings['pos' + str(i)] = list_to_string(position, ',')
+        shifted_position = [xyz + arb_shift for xyz in position]
+        fractional_shift_strings['pos' + str(i)] = list_to_string(shifted_position, ',')
+
+    # ** unpacks the dictionary
+    input_noshift = input_string.format(**fractional_strings)
+    input_shift = input_string.format(**fractional_shift_strings)
+    return (input_noshift, input_shift)
+
+
+def boron_nitride_hex_translational_invariance_strings(named_result, arb_shift):
+    # Reference: https://materialsproject.org/materials/mp-984/
+    input_string = named_result + """ := xtb(
+         structure( fractional=[[B, {pos0:s}],
+                                [B, {pos1:s}],
+                                [N, {pos2:s}],
+                                [N, {pos3:s}]]
+                lattice( a = 2.51242804 angstrom
+                         c = 7.70726501 angstrom
+                         bravais = hexagonal
+                        )
+                  )
+      repulsive_cutoff = 40.0 bohr
+      overlap_cutoff = 40.0 bohr
+      h0_cutoff = 40.0 bohr
+      temperature = 0 kelvin
+      ewald_real_cutoff = 10 bohr
+      ewald_reciprocal_cutoff = 2
+      ewald_alpha = 0.5
+      monkhorst_pack = [2, 2, 2]
+      symmetry_reduction = false
+     ) """
+
+    positions = [[0.333333, 0.666667, 0.250000],
+                 [0.666667, 0.333333, 0.750000],
+                 [0.333333, 0.666667, 0.750000],
+                 [0.666667, 0.333333, 0.250000]]
 
     fractional_strings = {}
     fractional_shift_strings = {}
@@ -97,26 +173,15 @@ def rutile_translational_invariance():
         shifted_position = [xyz + arb_shift for xyz in position]
         fractional_shift_strings['pos'+str(i)] = list_to_string(shifted_position, ',')
 
-    # ** unpacks the dictionary
-    input_noshift = input_string.format(**fractional_strings).replace('\n', ' ')
-    input_shift = input_string.format(**fractional_shift_strings).replace('\n', ' ')
+    input_noshift = input_string.format(**fractional_strings)
+    input_shift = input_string.format(**fractional_shift_strings)
 
-    energy = []
-    for input in [input_noshift, input_shift]:
-        entos_command = [entos_exe, '--format', 'json', '-s', input]
-        entos_output = subprocess.check_output(entos_command)
-        result = json.loads(entos_output)
-        energy.append(result['bulk_rutile']['energy'])
-        print(energy)
-
-    return abs(energy[1] - energy[0])
+    return (input_noshift, input_shift)
 
 
-#TODO(Alex) Fill in skeleton routines
-# def diamond_translational_invariance():
-#     return abs(energy[1] - energy[0])
+# Fill in skeleton routines
 #
-# def boron_nitride_hex():
+# def diamond_translational_invariance():
 #     return abs(energy[1] - energy[0])
 #
 # def sodium_chloride():
@@ -130,21 +195,52 @@ def rutile_translational_invariance():
 #
 # def lead_sulphide():
 #     return abs(energy[1] - energy[0])
+#
+# graphite:
+# Looks like CIF contains asymmetric cell and vesta converts to conventional
+# Need primitive cell
 
 
+# -----------------------------------------------
+# Generate entos input files
+# -----------------------------------------------
+
+def boron_nitride_input_str():
+    named_result = 'bn_hex'
+    (input1, input2) = boron_nitride_hex_translational_invariance_strings(named_result, arb_shift=0.13)
+    assert_string = " assert(load = " + named_result + " variable = n_iterations value = 6)\n"
+    assert_string += " assert(load = " + named_result + " variable = energy value =  -9.431841) \n\n"
+    print(input1 + '\n' + assert_string + input2 + assert_string)
+
+if generate_app_test_inputs:
+    boron_nitride_input_str()
+
+
+# -----------------------------------------------
 # Compute energy differences
-energy_difference = collections.OrderedDict()
+# -----------------------------------------------
 
-energy_difference['silicon'] = silicon_translational_invariance()
+if run_energy_differences:
+    energy_difference = collections.OrderedDict()
 
-energy_difference['rutile'] = rutile_translational_invariance()
+    energy_difference['silicon'] = get_energy_differences("silicon",
+                                                          silicon_translational_invariance_strings,
+                                                          arbitrary_shift=0.43)[0]
 
-# Evaluate energy differences
-for key, value in energy_difference:
-    if energy_difference > 1.e-6:
-        print(key + ' primitive unit cell')
-        print("Energy should be invariant w.r.t. translation of basis positions,"
-              "however we find |delta E| (Ha) = ", value)
+    energy_difference['rutile'] = get_energy_differences("rutile",
+                                                         rutile_translational_invariance_strings,
+                                                         arbitrary_shift=0.134)[0]
+
+    energy_difference['bn_hex'] = get_energy_differences("bn_hex",
+                                                         boron_nitride_hex_translational_invariance_strings,
+                                                         arbitrary_shift=0.13)[0]
+
+    all_pass = evaluate_energy_differences(energy_difference)
+    assert all_pass
+
+
+
+
 
 
 
