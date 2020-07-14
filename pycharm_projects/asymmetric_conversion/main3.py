d5b03605793532e1a019fc303e81fd3acbe17076
Alex Buccheri <alexanderbuccheri@googlemail.com>
2020-07-14 16:27:01 +0100

    For a given unit cell, list all surrounding atoms that neighbour atoms in the central cell

--- pycharm_projects/asymmetric_conversion/main3.py
+++ pycharm_projects/asymmetric_conversion/main3.py
@@ -84,13 +84,57 @@ n_atoms_super = np.product(n) * n_atoms_prim
 print("N atoms in primitive cell ", n_atoms_prim)
 print("N atoms in supercell: ",n_atoms_super)
 
+
+# --------------------------------------------------------------
+# For a given unit cell, list all surrounding atoms too
+# such that every atom in the central cell is fully coordinated
+# --------------------------------------------------------------
+
+# Move central cell translation vector to start of list
+translations.pop(int(0.5*len(translations)))
+translations = [np.array([0, 0, 0])] + translations
+
+# Unit cell that is fully coordinated
 visualise = False
+super_cell = supercell.build_supercell(unit_cell, translations)
+assert len(super_cell) == np.product(n) * n_atoms_prim
 if visualise:
-    s_cell = supercell.build_supercell(unit_cell, translations)
-    # Output it in xyz
-    # alex_xyz(output_dir + '/' + "aei_supercell", s_cell)
-    n_atoms_super = len(s_cell)
-    assert n_atoms_super == np.product(n) * n_atoms_prim
+    alex_xyz(output_dir + '/' + "aei_supercell", super_cell)
+
+positions = [atom.position for atom in super_cell]
+d_supercell = spatial.distance_matrix(positions, positions)
+assert d_supercell.shape[0] == len(super_cell) and d_supercell.shape[1] == len(super_cell)
+upper_bound_length = 1.8
+
+# For atoms (ia) in central cell, check for neighbours outside of central cell
+coordinating_atom_indices = []
+for ia in range(0, n_atoms_prim):
+    # This doesn't give the correct answer and I can't see why
+    # indices = np.where((d_supercell[ia, n_atoms_prim:] > 0.) &
+    #                    (d_supercell[ia, n_atoms_prim:] <= upper_bound_length))[0]
+    indices = np.where((d_supercell[ia, :] > 0.) &
+                       (d_supercell[ia, :] <= upper_bound_length))[0]
+    # Put these straight into the coordinating cell
+    coordinating_atom_indices.extend(indices)
+
+# Remove dups
+coordinating_atom_indices = list(set(coordinating_atom_indices))
+
+# Remove indices associated with atoms in central cell
+coordinating_atom_indices = np.asarray(coordinating_atom_indices)
+coordinating_atom_indices = coordinating_atom_indices[coordinating_atom_indices >= n_atoms_prim]
+
+# Store coordinating atoms
+coordinating_atoms = []
+for ia in coordinating_atom_indices:
+    coordinating_atoms.append(super_cell[ia])
+
+alex_xyz(output_dir + '/' + "aei_central_cell", unit_cell)
+alex_xyz(output_dir + '/' + "aei_neighbours", coordinating_atoms)
+
+quit()
+
+
 
 
 # -------------------------------------------------------------------------------
