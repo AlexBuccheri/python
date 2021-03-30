097c1d32c344f6ccb109e47b4bb556ceb3d2f76e
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-03-30 15:53:42 +0100

    Option to exclude nodes for a given job

--- exciting/job_schedulers/slurm.py
+++ exciting/job_schedulers/slurm.py
@@ -1,7 +1,7 @@
 """
 Generate SLURM script
 """
-from typing import Optional
+from typing import Optional, Dict, List
 from collections import OrderedDict
 
 
@@ -70,6 +70,30 @@ def time_to_string(time:list):
     return time_string
 
 
+def exclude_nodes(nodes: List[str]) -> str:
+    """
+    Return a string specifying nodes to exclude, of the form:
+        --exclude=node01,node02,node03
+
+    TODO For contiguous exclusions, one can compact the syntax
+    Return a string specifying nodes to exclude, of the form:
+        --exclude=node[01-03]
+    where 'node' is the name of the names and 01-09 is the range
+    of node id's to exclude.
+
+    Ref: https://stackoverflow.com/questions/26216727/how-to-submit-a-job-to-any-subset-of-nodes-from-nodelist-in-slurm
+
+    :param nodes: List of nodes to exclude
+    :return: string of nodes to exclude (see above)
+    """
+    exclude_string = ''
+
+    for node in nodes:
+        exclude_string += node + ','
+
+    return exclude_string[:-1]
+
+
 def set_slurm_directives(job_name:Optional[str]='default_name',
                          time:Optional[list]=None,
                          partition:Optional[str]=None,
@@ -77,19 +101,22 @@ def set_slurm_directives(job_name:Optional[str]='default_name',
                          nodes:Optional[int]=1,
                          ntasks_per_node:Optional[int]=None,
                          cpus_per_task:Optional[int]=None,
-                         hint:Optional[str]='nomultithread') -> OrderedDict:
+                         hint:Optional[str]='nomultithread',
+                         exclude:Optional[List]=None) -> OrderedDict:
     """
     Set ordered dictionary of slurm directives
+    TODO Add logic statements for when optionals are not present
 
-    :param job_name:
-    :param time:
-    :param partition:
-    :param exclusive:
-    :param nodes:
-    :param ntasks_per_node:
-    :param cpus_per_task:
-    :param hint:
-    :return:
+    :param job_name: Job name
+    :param time: Job Time, expected as a list
+    :param partition: Queue
+    :param exclusive: Allow different jobs to run on the same node?
+    :param nodes: Numbver of nodes
+    :param ntasks_per_node: Number of MPI ranks per node
+    :param cpus_per_task: Number of OMP threads per MPI rank
+    :param hint: Use hyperthreading
+    :param exclude: Nodes to exclude from use
+    :return: OrderedDict of slurm directives
     """
 
     time_str = time_to_string(time)
@@ -102,6 +129,10 @@ def set_slurm_directives(job_name:Optional[str]='default_name',
                                     ('cpus-per-task', str(cpus_per_task)),
                                     ('hint', hint)
                                     ])
+
+    if exclude is not None:
+        exclude_string = exclude_nodes(exclude)
+        slurm_directives['exclude'] = exclude_string
 
     # Add directives that don't have a RHS
     if exclusive:
