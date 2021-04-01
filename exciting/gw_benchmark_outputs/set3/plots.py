c15598392949c2a8fe1d09f8e769ee5169738b63
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-04-01 16:33:54 +0100

    Cleaned up and finished, such that it produces saved images for all data of interest

--- exciting/gw_benchmark_outputs/set3/plots.py
+++ exciting/gw_benchmark_outputs/set3/plots.py
@@ -1,270 +1,206 @@
 """
 Plots
 """
+# External libraries
 from collections import OrderedDict
 import numpy as np
 import matplotlib.pyplot as plt
 
-from parse.parse_gw import parse_gw_info, parse_gw_evalqp
-from process.process_gw import process_gw_gamma_point
+# My modules
 from units_and_constants.unit_conversions import ha_to_mev
+from ex_plot.plot import Plot
+from gw_benchmark_outputs.post_process_utils import parse_gw_results, get_basis_labels, combine_species_basis_labels, \
+    n_local_orbitals
 
 
-def directory_string(l_max:OrderedDict) -> str:
+def process_basis_numbers(delta_E_qp, max_energy_exts:list):
     """
-    Top level basis convergence directory string
+    Print change in QP gap for each max energy parameter (i.e. max LO)
     """
-    path = ''
-    for species, l in l_max.items():
-        path += species + '_lmax' + str(l) + '_'
-
-    return path
-
-# TODO(Alex) Duplicate of routine in all A1_zr_lmax_o_lmax scripts
-def restructure_energy_cutoffs(energy_cutoffs: dict) -> list:
-    """
-    Get in a more useful structure to iterate over
-
-    """
-    restructured_energies = []
-
-    for inum in range(0, 4):
-        data = {}
-        for species, l_channels in  energy_cutoffs.items():
-            data[species] = {l:energies[inum] for l, energies in l_channels.items()}
-        restructured_energies.append(data)
-
-    return restructured_energies
-
-
-def max_energy_ext_per_directory(energy_cutoffs):
-    """
-    Get the energy extension string for each run directory,
-    for a given set of calculations
-
-    :param energy_cutoffs: of the form
-
-    {'zr': {0: np.linspace(60, 120, num=4),
-            1: np.linspace(60, 120, num=4),
-            2: np.linspace(90, 300, num=4),
-            3: np.linspace(60, 120, num=4)},
-     'o':  {0: np.linspace(60, 120, num=4),
-            1: np.linspace(60, 120, num=4),
-            2: np.linspace(60, 120, num=4)}
-                       }
-    :return: max_energy_exts
-    """
-    max_energy_exts = []
-    for energy in restructure_energy_cutoffs(energy_cutoffs):
-        max_energy_per_species = [max(energy_per_l_channel.values()) for energy_per_l_channel in
-                                  energy.values()]
-        max_energy_exts.append( str(int(max(max_energy_per_species))) )
-    return max_energy_exts
-
-
-def parse_gw_results(root:str, settings:dict) -> dict:
-    """
-
-    QP direct-gap (relative to the KS gap)
-    and self-energies of the band edges at Gamma.
-
-    Almost easier to just path full directive strings.
-
-    :return: dictionary containing the above.
-    """
-
-    # Basis settings
-    rgkmax = settings['rgkmax']
-    l_max_values = settings['l_max_values']
-    max_energy_exts = settings['max_energy_cutoffs']
-
-    # GW settings
-    n_empty_ext = settings['n_empty_ext']
-    q_grid = settings['q_grid']
-    n_img_freq = settings['n_img_freq']
-
-    # Data to parse and return
-    delta_E_qp = np.empty(shape=(len(max_energy_exts), len(l_max_values)))
-    re_self_energy_VBM = np.empty(shape=delta_E_qp.shape)
-    re_self_energy_CBm = np.empty(shape=delta_E_qp.shape)
-    q_str = "".join(str(q) for q in q_grid)
-
-    # Lmax in LO basis
-    for i, l_max in enumerate(l_max_values):
-        basis_root = root + '/' + directory_string(l_max) + 'rgkmax' + str(rgkmax)
-        gw_root = basis_root + "/gw_q" + q_str + "_omeg" + str(n_img_freq) + "_nempty" + str(n_empty_ext[i])
-
-        # Max energy cut-off of LOs in each l-channel
-        for ienergy, energy in enumerate(max_energy_exts):
-            file_path = gw_root + '/max_energy_' + str(energy)
-
-            gw_data = parse_gw_info(file_path)
-            qp_data = parse_gw_evalqp(file_path)
-
-            print('Reading data from ', file_path)
-            results = process_gw_gamma_point(gw_data, qp_data)
-            delta_E_qp[ienergy, i] = results['E_qp'] - results['E_ks']
-            re_self_energy_VBM[ienergy, i] = results['re_sigma_VBM']
-            re_self_energy_CBm[ienergy, i] = results['re_sigma_CBm']
-
-    return {'delta_E_qp': delta_E_qp,
-            're_self_energy_VBM': re_self_energy_VBM,
-            're_self_energy_CBm': re_self_energy_CBm
-            }
-
-
-
-class PlotGW:
-
-    def __init__(self, x, y, x_label='', y_label='', xticklabels=None, yticklabels=None, legend_loc='upper right', legend_title=None):
-        self.x = x
-        self.y = y
-        self.x_label = x_label
-        self.y_label = y_label
-        self.xticklabels = xticklabels
-        self.yticklabels = yticklabels
-        self.legend_loc = legend_loc
-        self.legend_title = legend_title
-        self.initialise_plot()
-
-    def initialise_plot(self):
-        """
-        Initialise plot axes and labels
-        :return:
-        """
-        self.fig , self.ax = plt.subplots()
-        self.ax.set_xlabel(self.x_label)
-        self.ax.set_ylabel(self.y_label)
-
-        if self.xticklabels is not None:
-            self.ax.set_xticks(self.x)
-            self.ax.set_xticklabels(self.xticklabels)
-
-        if self.yticklabels is not None:
-            self.ax.set_yticks(self.y)
-            self.ax.set_yticklabels(self.yticklabels)
-
-        return
-
-    def plot_data(self, x, y, label=None):
-        """
-        Basic plot wrapper
-        :param x: x data
-        :param y: y data
-        :param label: optional label for legend
-        """
-        if label is None:
-            self.ax.plot(x, y)
-        else:
-            self.ax.plot(x, y, label=str(label))
-        return
-
-    def show(self, file_name=None):
-        if file_name is not None:
-            self.ax.legend(loc=self.legend_loc, title=self.legend_title)
-            plt.savefig(file_name, dpi=300)
-        else:
-            self.ax.legend(loc=self.legend_loc, title=self.legend_title)
-            plt.show()
-        return
-
-
-
-def process_basis_numbers(l_max_values, delta_E_qp, max_energy_exts:list):
-    """
-    Print information of interest
-    """
-
-    # For (4,3), change in energy for each energy parameter
-    lo_43 = 1
+    assert delta_E_qp.size == len(max_energy_exts), 'Should be same number of QP energies as there are basis energy cutoffs'
     print("For basis (Zr,O) = (4,3)")
     for ie, energy in enumerate(max_energy_exts[1:], start=1):
-        change_in_delta_E_qp = (delta_E_qp[ie, lo_43] - delta_E_qp[ie-1, lo_43]) * ha_to_mev
+        change_in_delta_E_qp = (delta_E_qp[ie] - delta_E_qp[ie-1])
         print("Change in Delta E_QP from max energy param ", max_energy_exts[ie-1], "to ", energy, ":", change_in_delta_E_qp, "(meV)")
-
     return
+
+
+def sum_los_per_species(n_los_species_resolved: dict) ->list:
+    """
+    Sum N LOs for each species, per calculation.
+
+    :return: List, n_los, containing the number of LOs per calculation.
+    """
+    species = [key for key in n_los_species_resolved.keys()]
+    n_energy_cutoffs = len(n_los_species_resolved[species[0]])
+
+    n_los = []
+    for i in range(0, n_energy_cutoffs):
+        n = 0
+        for element in species:
+            n += n_los_species_resolved[element][i]
+        n_los.append(n)
+
+    return n_los
 
 
 def gw_basis_convergence(root:str):
     """
-    Plot the quasipartilce-KS gap w.r.t basis l-max, for
-    several max lo energies.
-    NOTE: n_empty_ext corresponds to the directory extensions
+    Plot the quasipartilce-KS gap w.r.t bs LO basis, for l-max = (4, 3(
     """
     D = OrderedDict
 
-    # Set 2 results
-    data2 = parse_gw_results(root + "/A1_var_cutoff",
-                                   {'rgkmax': 7,
-                                    'l_max_values': [D([('zr', 4), ('o', 3)])],
-                                    'n_img_freq': 32,
-                                    'q_grid': [2, 2, 2],
-                                    'n_empty_ext': [1000],
-                                    'max_energy_cutoffs': [90, 160, 230, 300]})
-    delta_E_qp2 = data2['delta_E_qp']
-
-    data3 = parse_gw_results(root + "/A1_set3",
-                                   {'rgkmax': 7,
-                                    'l_max_values': [D([('zr', 4), ('o', 3)])],
-                                    'n_img_freq': 32,
-                                    'q_grid': [2, 2, 2],
-                                    'n_empty_ext': [1000],
-                                    'max_energy_cutoffs': ['i0', 'i1', 'i2', 'i4', 'i5', 'i6', 'i7']})   # 'i3']
-    # i7. GW VBT and CBB get fucked up
-
-    delta_E_qp3 = data3['delta_E_qp']
-
-    print(delta_E_qp2 * ha_to_mev)
-    print(delta_E_qp3 * ha_to_mev)
-
-    # Make QP vs basis size (total basis size)
-
+    lmax_43 = 0
     plot_delta_E_qp = True
     plot_sigma = True
+    save_plots = True
 
-    # -----------------
-    # Plot delta_E_qp
-    # -----------------
+    # Set 2
+    max_energy_exts_set2 = [90, 160, 230, 300]
+    settings_set2 =  {'rgkmax': 7,
+                      'l_max_values': [D([('zr', 4), ('o', 3)])],
+                      'n_img_freq': 32,
+                      'q_grid': [2, 2, 2],
+                      'n_empty_ext': [1000],
+                      'max_energy_cutoffs': max_energy_exts_set2}
+
+    data_set2 = parse_gw_results(root + "/A1_var_cutoff", settings_set2)
+    delta_E_qp_set2 = data_set2['delta_E_qp'][:, lmax_43] * ha_to_mev
+    basis_labels = get_basis_labels(root + "/A1_var_cutoff", settings_set2)
+    basis_labels_set2 = combine_species_basis_labels(basis_labels,  species_per_line=True)['(4,3)']
+    n_los_set2 = n_local_orbitals(basis_labels)['(4,3)']
+
+    # Set 3
+    # i3 wouldn't work and i7. GW VBT and CBB get fucked up, hence omitted
+    #                              i0    i1   i2   i4   i5   i6
+    # energy_cutoffs = {'zr': {0: [150, 200, 250, 200, 120, 120],
+    #                          1: [150, 200, 250, 200, 120, 120],
+    #                          2: [300, 300, 300, 350, 350, 400],
+    #                          3: [150, 200, 250, 200, 120, 120],
+    #                          4: [150, 200, 250, 200, 120, 120]},
+    #
+    #                    'o': {0: [150, 200, 250, 200, 120, 120],
+    #                          1: [150, 200, 250, 200, 120, 120],
+    #                          2: [150, 200, 250, 200, 120, 120],
+    #                          3: [150, 200, 250, 200, 120, 120]}
+    #                   }
+
+    max_energy_exts_set3 = ['i0', 'i1', 'i2', 'i4', 'i5', 'i6']
+    settings_set3 = {'rgkmax': 7,
+                     'l_max_values': [D([('zr', 4), ('o', 3)])],
+                     'n_img_freq': 32,
+                     'q_grid': [2, 2, 2],
+                     'n_empty_ext': [1000], # Note in some cases, I actually used 2000 in input
+                     'max_energy_cutoffs':max_energy_exts_set3 }
+
+    data_set3 = parse_gw_results(root + "/A1_set3", settings_set3)
+    delta_E_qp_set3 = data_set3['delta_E_qp'][:, lmax_43] * ha_to_mev
+    basis_labels = get_basis_labels(root + "/A1_set3", settings_set3)
+    n_los_set3 = n_local_orbitals(basis_labels)['(4,3)']
+    basis_labels_set3 = combine_species_basis_labels(basis_labels,  species_per_line=True)['(4,3)']
+
+
+    # Give some changes in QP gap w.r.t. calculations
+    process_basis_numbers(delta_E_qp_set2, max_energy_exts_set2)
+    process_basis_numbers(delta_E_qp_set3, max_energy_exts_set3)
+
+    # Total number of LOs per calculation (i.e. sum Zr and O basis sizes together)
+    n_los_set2 = sum_los_per_species(n_los_set2)
+    n_los_set3 = sum_los_per_species(n_los_set3)
+
+    # --------------------------------------
+    # QP vs LO basis size, for l-max = (4,3)
+    # --------------------------------------
     if plot_delta_E_qp:
-        x = np.arange(0, len(l_max_values))
-        x_labels = ["(" + ",".join(str(l) for l in l_pair.values()) + ")" for l_pair in l_max_values]
 
-        gw_plot = PlotGW(x, [], x_label='l_max (Zr, O)', y_label='Quasiparticle Gap - KS Gap at Gamma (meV)',
-                         xticklabels=x_labels, legend_title='Max LO Energy Param (Ha)')
+        # Set 2 and 3
+        fig, ax = plt.subplots()
+        ax.set_xlabel('N LOs')
+        ax.set_ylabel('Quasiparticle Gap - KS Gap at Gamma (meV)')
 
-        for i, energy in enumerate(max_energy_exts):
-            gw_plot.plot_data(x, delta_E_qp[i, :] * ha_to_mev, label=str(energy))
+        ax.plot(n_los_set2, delta_E_qp_set2, color='blue', marker='o', linestyle='solid', linewidth=3, markersize=8)
+        for i, txt in enumerate(basis_labels_set2):
+           ax.annotate(txt, (n_los_set2[i], delta_E_qp_set2[i]))
 
-        gw_plot.show()
+        ax.plot(n_los_set3, delta_E_qp_set3, 'ro', markersize=8)
 
+        if save_plots:
+            plt.savefig('qp_convergence_lmax43.ps', dpi=300, facecolor='w', edgecolor='w',
+                        orientation='portrait', papertype=None, transparent=True, bbox_inches=None, pad_inches=0.1)
+        plt.show()
 
-    quit()
+        # Set 3 by itself, where one is primarily looking at the l=2 channel of Zr.
+        for i in range(0, delta_E_qp_set3.size):
+            print(max_energy_exts_set3[i], delta_E_qp_set3[i])
 
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 10)
+        plt.rcParams.update({'font.size': 11})   # Affects fonts of the labels only
+        ax.tick_params(axis='both', which='major', labelsize=14)
 
+        ax.set_xlim(80, 125)
+        ax.set_ylim(1580, 1660)
+        ax.set_xlabel('N LOs', fontsize=16)
+        ax.set_ylabel('Quasiparticle Gap - KS Gap at Gamma (meV)', fontsize=16)
+        ax.plot(n_los_set3, delta_E_qp_set3, 'ro', markersize=10)
+        for i, txt in enumerate(basis_labels_set3):
+            ax.annotate(txt, (n_los_set3[i], delta_E_qp_set3[i]))
 
+        # Note, can use plt. or fig. to save and show
+        if save_plots:
+            # ALso note that ps cuts off the edges, hence using jpeg
+            plt.savefig('qp_convergence_set3_lmax43.jpeg', dpi=300, facecolor='w', edgecolor='w',
+                        orientation='portrait', transparent=True, bbox_inches=None, pad_inches=0.1)
 
-
+        plt.show()
 
     # ----------------------------------------------------------------------
-    # Plot VBT and CBB real self-eneries w.r.t. basis and energy parameter
+    # Plot VBT and CBB real self-eneries w.r.t. LO energy cut-off
+    # for set 3, only
     # ----------------------------------------------------------------------
-    # if plot_sigma:
-    #     re_self_energy_VBM = data['re_self_energy_VBM']
-    #     r_sigma_VBM_plot = PlotGW(x, [], x_label='l_max (Zr, O)', y_label='Re{Self Energy} at VBM (meV) ',
-    #                               xticklabels=x_labels, legend_title='Max LO Energy Param (Ha)')
-    #
-    #     for i, energy in enumerate(max_energy_exts):
-    #         r_sigma_VBM_plot.plot_data(x, re_self_energy_VBM[i, :] * ha_to_mev, label=str(energy))
-    #     r_sigma_VBM_plot.show()
-    #
-    #     re_self_energy_CBm = data['re_self_energy_CBm']
-    #     r_sigma_CBm_plot = PlotGW(x, [], x_label='l_max (Zr, O)', y_label='Re{Self Energy} at CBm (meV) ',
-    #                               xticklabels=x_labels, legend_title='Max LO Energy Param (Ha)')
-    #
-    #     for i, energy in enumerate(max_energy_exts):
-    #         r_sigma_CBm_plot.plot_data(x, re_self_energy_CBm[i, :] * ha_to_mev, label=str(energy))
-    #     r_sigma_CBm_plot.show()
+    if plot_sigma:
 
+        # What we expect: VBT converges with the oxygen LOs, and is well-converged
+        re_self_energy_VBM = data_set3['re_self_energy_VBM'][:, lmax_43] * ha_to_mev
+        fig, ax = plt.subplots()
+
+        fig.set_size_inches(12, 10)
+        plt.rcParams.update({'font.size': 11})
+        ax.tick_params(axis='both', which='major', labelsize=14)
+
+        ax.set_xlim(82, 120)
+        ax.set_xlabel('N LOs', fontsize=16)
+        ax.set_ylabel('Re{Self Energy} at VBM (meV)', fontsize=16)
+        ax.plot(n_los_set3, re_self_energy_VBM, 'ro', markersize=10)
+        for i, txt in enumerate(basis_labels_set3):
+            ax.annotate(txt, (n_los_set3[i], re_self_energy_VBM[i]))
+
+        if save_plots:
+            plt.savefig('VBM_convergence_set3_lmax43.jpeg', dpi=300, facecolor='w', edgecolor='w',
+                        orientation='portrait', transparent=True, bbox_inches=None, pad_inches=0.1)
+
+        plt.show()
+
+        # CBm is strongly dependent on the number of Zr LOs, and is still not converged.
+        re_self_energy_CBm = data_set3['re_self_energy_CBm'][:, lmax_43] * ha_to_mev
+        fig, ax = plt.subplots()
+
+        fig.set_size_inches(12, 10)
+        plt.rcParams.update({'font.size': 11})
+        ax.tick_params(axis='both', which='major', labelsize=14)
+
+        ax.set_xlim(82, 120)
+        ax.set_xlabel('N LOs', fontsize=16)
+        ax.set_ylabel('Re{Self Energy} at CBm (meV)', fontsize=16)
+        ax.plot(n_los_set3, re_self_energy_CBm, 'ro', markersize=10)
+        for i, txt in enumerate(basis_labels_set3):
+            ax.annotate(txt, (n_los_set3[i], re_self_energy_CBm[i]))
+
+        if save_plots:
+            plt.savefig('CBm_convergence_set3_lmax43.jpeg', dpi=300, facecolor='w', edgecolor='w',
+                        orientation='portrait', transparent=True, bbox_inches=None, pad_inches=0.1)
+
+        plt.show()
 
     return
 
