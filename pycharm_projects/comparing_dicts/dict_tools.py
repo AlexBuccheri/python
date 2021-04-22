5b75246e487361a8cb82451b4d06f965b724a2f6
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-04-22 14:13:39 +0100

    More elaboration in the docs

--- pycharm_projects/comparing_dicts/dict_tools.py
+++ pycharm_projects/comparing_dicts/dict_tools.py
@@ -10,14 +10,17 @@ def get_hashable_entries(nested:dict):
     Create a dictionary of all hashable values, removing nesting.
 
     Note, this will fail if a cross edge or a back edge is included in the dict
-    (i.e. a self-reference of the form ...). However, for the use case of flattening parsed results,
-    this should never be an issue.
+    i.e. a self-reference of the form:
+
+    A = {key1:value, key2:A}
+
+    However, for the use case of flattening parsed results, this should never be an issue.
 
     # TODO Question of a) how robust is this and b) how fast is it?
     https://stackoverflow.com/questions/10756427/loop-through-all-nested-dictionary-values
 
-    :param nested:
-    :return:
+    :param nested: Nested dictionary with a mix and hashable and non-hashable values.
+    :return: Generator
     """
     for key, value in nested.items():
         if isinstance(value, abc.Mapping):
@@ -26,6 +29,8 @@ def get_hashable_entries(nested:dict):
             yield (key, value)
 
 
+# TODO This should be replaced by a check of key-consistency in the nested dicts
+# Maybe easy to do with deepdiff
 def check_key_consistency(a:list, b:list) -> bool:
     keys_a = [dictionary.keys() for dictionary in a]
     keys_b = [dictionary.keys() for dictionary in b]
@@ -37,12 +42,17 @@ def compare_result_with_reference(parsed_result:dict, parsed_reference:dict, tol
     # TODO Fill missing tolerances with defaults
 
     Provides some way of comparing nested data from two dictionaries.
-    It takes all hashable entries and puts them sequentially into a list, of the form:
 
-        [{key1:value1}, {key2:value2}, {key3:value3}, ...]
+    It takes all hashable entries:
 
-    Note, this is not serialising the data. In flattening the dictionary, we lose some key information.
-    This is acceptable if the reference and result data have the same structure.
+    dict = { key1:value1, key2:value2, key3:{key4:value4}}
+
+    and puts them sequentially into a list, of the form:
+
+        [{key1:value1}, {key2:value2}, {key4:value4}]
+
+    Note, this is not serialising the data. In flattening the dictionary, we lose some key information
+    (key3 in the above example). This is acceptable if the reference and result data have the same structure.
     As such, some pre-check that the keys of the nested dictionaries are consistent before comparing flattened data
     is required.
 
@@ -50,22 +60,18 @@ def compare_result_with_reference(parsed_result:dict, parsed_reference:dict, tol
     Only likely to occur if structure of output or parser changes, and the reference data has not bee updated.
     Can just be run in debug mode.
 
-    If an assertion fails, the code crashes.
-
     Not sure if there's a way of making this work for two differing nested structures.
     i.e. if we switch to NOMAD parser, existing references break, if we switch to structured output (removing
     the need for NOMAD parsers, because parsing becomes trivial), existing references also break.
     Implying we need to generate new reference data each time we change parser OR structure of output data
+
+    If an assertion fails, the script fails.
 
     :param dict result: results
     :param dict reference: references
     :param dict tolerance: tolerances for result to be considered equal to reference
     :param bool debug_mode: Perform debug mode checks
     """
-
-    # Note, this approach will overwrite values if any key is found twice in the nested dictionary
-    #result = {key: value for key, value in get_hashable_entries(parsed_result)}
-    #reference = {key: value for key, value in get_hashable_entries(parsed_reference)}
 
     result = [{key: value} for key, value in get_hashable_entries(parsed_result)]
     reference = [{key: value} for key, value in get_hashable_entries(parsed_reference)]
