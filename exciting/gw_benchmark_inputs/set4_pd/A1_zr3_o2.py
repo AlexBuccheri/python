1076572332c5c903ff421088e8adb280796079fe
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-05-30 19:00:14 +0200

    Cleaned up set4_pd - verified the plotting works on the existing data, and added some info to print out

---
+++ exciting/gw_benchmark_inputs/set4_pd/A1_zr3_o2.py
@@ -0,0 +1,103 @@
+"""
+Given some converged ground state calculation set up GW calculations
+with optimised bases, for a range of energy parameter cutoffs.
+"""
+from pathlib import Path
+import shutil
+from collections import OrderedDict
+from distutils.dir_util import copy_tree
+
+from parse.lorecommendations_parser import parse_lorecommendations
+from parse.parse_linengy import parse_lo_linear_energies
+from parse.parse_basis_xml import parse_basis_as_string
+from parse.set_gw_input import GWInput, set_gw_input_string
+from process.optimised_basis import DefaultLOs, filter_lo_functions, generate_optimised_basis_string
+from job_schedulers import slurm
+
+from gw_benchmark_inputs.input_utils import write_file, write_input_file_with_gw_settings, write_optimised_lo_basis, \
+    restructure_energy_cutoffs
+
+from gw_benchmark_inputs.set4_pd.A1_groundstate import converged_ground_state_input as A1_gs_input
+
+
+def set_up_g0w0(root_path: str):
+
+    # Material
+    species = ['zr', 'o']
+    l_max = {'zr': 4, 'o': 3}
+
+    gw_root = write_input_file_with_gw_settings(root_path,
+                                                A1_gs_input,
+                                                GWInput(taskname="g0w0", nempty=2000, ngridq=[2, 2, 2], skipgnd=False,
+                                                        n_omega=32)
+                                                )
+
+    # Default basis settings
+    default_linear_energies = parse_lo_linear_energies(root_path + "/groundstate")
+    default_los = {'zr': DefaultLOs(default_linear_energies['zr'], energy_tol=1.5),
+                   'o': DefaultLOs(default_linear_energies['o'], energy_tol=1.5)}
+
+    # Default basis strings with .format tags
+    default_basis_string = {'zr': parse_basis_as_string(root_path + "/groundstate/Zr.xml"),
+                            'o': parse_basis_as_string(root_path + "/groundstate/O.xml")}
+
+    # LO recommendation energies
+    lorecommendations = parse_lorecommendations(root_path + '/lorecommendations.dat', species)
+
+    # Increased rgkmax to 8                                   Zrl0   l2   l3   l4
+    energy_cutoffs = {'zr': {0: [180, 180, 180, 180, 180, 180, 300, 180, 180, 180],
+                             1: [180, 200, 250, 300, 350, 400, 180, 180, 180, 180],
+                             2: [180, 180, 180, 180, 180, 180, 180, 400, 180, 180],
+                             3: [180, 180, 180, 180, 180, 180, 180, 180, 420, 180]},
+
+                      'o': {0: [140, 140, 140, 140, 140, 140, 140, 140, 140, 140],
+                            1: [140, 140, 140, 140, 140, 140, 140, 140, 140, 140],
+                            2: [140, 140, 140, 140, 140, 140, 140, 140, 140, 140]}
+                      }
+
+    # Slurm script settings
+    env_vars = OrderedDict([('EXE', '/users/sol/abuccheri/exciting/bin/excitingmpismp'),
+                            ('OUT', 'terminal.out')
+                            ])
+    module_envs = ['intel/2019']
+    slurm_directives = slurm.set_slurm_directives(time=[0, 24, 0, 0],
+                                                  partition='all',
+                                                  exclusive=True,
+                                                  nodes=4,
+                                                  ntasks_per_node=2,
+                                                  cpus_per_task=18,
+                                                  hint='nomultithread')
+
+    species_basis_string = "".join(s.capitalize() + str(l_max[s]) + '_' for s in species)
+
+    for ie, energy_cutoff in enumerate(restructure_energy_cutoffs(len(energy_cutoffs['zr'][0]), energy_cutoffs)):
+
+        # Copy ground state directory to GW directory
+        # Use an index not max energy, as the max energy does not change in 3/4 runs
+        job_dir = gw_root + '/max_energy_i' + str(ie)
+        print('Creating directory, with input.xml, run.sh and optimised basis:', job_dir)
+        print(root_path)
+        copy_tree(root_path + '/groundstate', job_dir)
+
+        # Copy input.xml with GW settings
+        shutil.copy(gw_root + "/input.xml", job_dir + "/input.xml")
+
+        # New Slurm script
+        slurm_directives['job-name'] = "gw_A1_lmax_" + species_basis_string + str(ie) + 'loEcutoff'
+        write_file(job_dir + '/run.sh', slurm.set_slurm_script(slurm_directives, env_vars, module_envs))
+
+        # Write optimised basis
+        write_optimised_lo_basis('zr', l_max['zr'], energy_cutoff['zr'], lorecommendations['zr'],
+                                 default_basis_string['zr'], default_los['zr'], job_dir)
+        write_optimised_lo_basis('o', l_max['o'], energy_cutoff['o'], lorecommendations['o'],
+                                 default_basis_string['o'], default_los['o'], job_dir)
+
+    return
+
+if __name__ == "__main__":
+    # This does two things:
+    #  i0 - i5 converge the p-channel of Zr
+    #  i6 - i9 check the sensitivity of large values in check l-channel
+    set_up_g0w0("/users/sol/abuccheri/gw_benchmarks/A1_more_APW/set4_pd/zr_lmax3_o_lmax2_rgkmax8")
+
+
---
+++ exciting/gw_benchmark_outputs/set4_pd/plots.py
@@ -0,0 +1,166 @@
+"""
+Plots
+"""
+# External libraries
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+# My modules
+from units_and_constants.unit_conversions import ha_to_mev
+from gw_benchmark_outputs.post_process_utils import parse_gw_results, get_basis_labels, combine_species_basis_labels, \
+    n_local_orbitals
+
+
+def process_basis_numbers(delta_E_qp, max_energy_exts:list):
+    """
+    Print change in QP gap for each max energy parameter (i.e. max LO)
+    """
+    assert delta_E_qp.size == len(max_energy_exts), 'Should be same number of QP energies as there are basis energy cutoffs'
+
+    print("For basis (Zr,O) = (4,3)")
+    for ie, energy in enumerate(max_energy_exts[1:], start=1):
+        change_in_delta_E_qp = (delta_E_qp[ie] - delta_E_qp[ie-1])
+        print("Change in Delta E_QP from max energy param: ")
+        print(max_energy_exts[ie-1].replace("\n", " "), "to ")
+        print(energy.replace("\n", " "), ":")
+        print(change_in_delta_E_qp, "(meV)")
+    return
+
+
+def sum_los_per_species(n_los_species_resolved: dict) ->list:
+    """
+    Sum N LOs for each species, per calculation.
+
+    :return: List, n_los, containing the number of LOs per calculation.
+    """
+    species = [key for key in n_los_species_resolved.keys()]
+    n_energy_cutoffs = len(n_los_species_resolved[species[0]])
+
+    n_los = []
+    for i in range(0, n_energy_cutoffs):
+        n = 0
+        for element in species:
+            n += n_los_species_resolved[element][i]
+        n_los.append(n)
+
+    return n_los
+
+
+def gw_basis_convergence(root: str):
+    """
+    Plot the quasi particle - KS gap w.r.t bs LO basis, for l-max = (4, 3)
+    """
+    D = OrderedDict
+
+    info = """  This run does two things:
+    i0 - i5 converge the p-channel of Zr
+    i6 - i9 check the sensitivity of large values in check l-channel
+    
+    One sees that Zr d is already converged at 11 LOs, and Zr-f is converged at 10 LOs
+    Some sensitivity w.r.t the Zr-p, within ~ 4 meV spread. p at 11 LOs looks converged to ~ 2 meV
+    For increasing Zr-s from 9 to 12 LOs, we get the outlier at the bottom of the plot: i.e. ~ 7 meV change
+    As such, the calculation following this one should take the highest s out of the core and converge the Zr-s channel
+    """
+
+    print(info)
+
+    lmax_32 = 0
+    plot_delta_E_qp = True
+    plot_sigma = False
+    save_plots = False
+
+    max_energy_exts_set4_pd = ['i0', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9']
+
+    settings_set4_pd = {'rgkmax': 8,
+                        'l_max_values': [D([('zr', 3), ('o', 2)])],
+                        'n_img_freq': 32,
+                        'q_grid': [2, 2, 2],
+                        'n_empty_ext': [2000],
+                        'max_energy_cutoffs': max_energy_exts_set4_pd
+                       }
+
+    data_set4 = parse_gw_results(root, settings_set4_pd)
+    delta_E_qp_set4 = data_set4['delta_E_qp'][:, lmax_32] * ha_to_mev
+    basis_labels = get_basis_labels(root, settings_set4_pd, verbose=True)
+    basis_labels_set4 = combine_species_basis_labels(basis_labels, species_per_line=True)['(3,2)']
+    n_los_set4 = n_local_orbitals(basis_labels)['(3,2)']
+
+    # Give some changes in QP gap w.r.t. calculations
+    print(delta_E_qp_set4)
+    process_basis_numbers(delta_E_qp_set4, basis_labels_set4)
+
+    # Total number of LOs per calculation (i.e. sum Zr and O basis sizes together)
+    n_los_set4 = sum_los_per_species(n_los_set4)
+
+    # --------------------------------------
+    # QP vs LO basis size, for l-max = (4,3)
+    # --------------------------------------
+    if plot_delta_E_qp:
+
+        fig, ax = plt.subplots()
+        ax.set_xlabel('N LOs')
+        ax.set_ylabel('Quasiparticle Gap - KS Gap at Gamma (meV)')
+
+        ax.plot(n_los_set4, delta_E_qp_set4, color='blue', marker='o', markersize=8)
+        for i, txt in enumerate(basis_labels_set4):
+           ax.annotate(txt, (n_los_set4[i], delta_E_qp_set4[i]))
+
+        if save_plots:
+            plt.savefig('qp_convergence_lmax43.ps', dpi=300, facecolor='w', edgecolor='w',
+                        orientation='portrait', papertype=None, transparent=True, bbox_inches=None, pad_inches=0.1)
+        plt.show()
+
+
+    # ----------------------------------------------------------------------
+    # Plot VBT and CBB real self-energies w.r.t. LO energy cut-off
+    # for set 3, only
+    # ----------------------------------------------------------------------
+    if plot_sigma:
+        print("Requires rewriting")
+        pass
+        # # What we expect: VBT converges with the oxygen LOs, and is well-converged
+        # re_self_energy_VBM = data_set4['re_self_energy_VBM'][:, lmax_32] * ha_to_mev
+        # fig, ax = plt.subplots()
+        #
+        # fig.set_size_inches(12, 10)
+        # plt.rcParams.update({'font.size': 11})
+        # ax.tick_params(axis='both', which='major', labelsize=14)
+        #
+        # ax.set_xlim(82, 120)
+        # ax.set_xlabel('N LOs', fontsize=16)
+        # ax.set_ylabel('Re{Self Energy} at VBM (meV)', fontsize=16)
+        # ax.plot(n_los_set4, re_self_energy_VBM, 'ro', markersize=10)
+        # for i, txt in enumerate(basis_labels_set4):
+        #     ax.annotate(txt, (n_los_set4[i], re_self_energy_VBM[i]))
+        #
+        # if save_plots:
+        #     plt.savefig('VBM_convergence_set3_lmax43.jpeg', dpi=300, facecolor='w', edgecolor='w',
+        #                 orientation='portrait', transparent=True, bbox_inches=None, pad_inches=0.1)
+        #
+        # plt.show()
+        #
+        # # CBm is strongly dependent on the number of Zr LOs, and is still not converged.
+        # re_self_energy_CBm = data_set4['re_self_energy_CBm'][:, lmax_32] * ha_to_mev
+        # fig, ax = plt.subplots()
+        #
+        # fig.set_size_inches(12, 10)
+        # plt.rcParams.update({'font.size': 11})
+        # ax.tick_params(axis='both', which='major', labelsize=14)
+        #
+        # ax.set_xlim(82, 120)
+        # ax.set_xlabel('N LOs', fontsize=16)
+        # ax.set_ylabel('Re{Self Energy} at CBm (meV)', fontsize=16)
+        # ax.plot(n_los_set4, re_self_energy_CBm, 'ro', markersize=10)
+        # for i, txt in enumerate(basis_labels_set4):
+        #     ax.annotate(txt, (n_los_set4[i], re_self_energy_CBm[i]))
+        #
+        # if save_plots:
+        #     plt.savefig('CBm_convergence_set3_lmax43.jpeg', dpi=300, facecolor='w', edgecolor='w',
+        #                 orientation='portrait', transparent=True, bbox_inches=None, pad_inches=0.1)
+        #
+        # plt.show()
+
+    return
+
+
+gw_basis_convergence("/users/sol/abuccheri/gw_benchmarks/A1_more_APW/set4_pd")
