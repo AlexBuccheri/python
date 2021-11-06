77f41f2fec0368ea0d008f78414b101df712c7af
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-11-06 16:25:12 +0000

    Clean up test

--- exciting/job_schedulers/test_pbs_pro.py
+++ exciting/job_schedulers/test_pbs_pro.py
@@ -5,44 +5,26 @@ from pbs_pro import set_pbs_pro_directives, set_pbs_pro
 
 def test_set_pbs_pro_without_optionals():
 
-    ref = """#!/bin/bash 
+    omp = 16
+    directives = set_pbs_pro_directives(time=[24,00,0],
+                                        queue_name='single',
+                                        nodes=1,
+                                        mpi_ranks_per_node=8,
+                                        omp_threads_per_process=omp,
+                                        cores_per_node=128,
+                                        node_type='rome',
+                                        job_name='GW_gs')
 
-#PBS -N HAWK Rome Pure MPI
-#PBS -q ADD ME
-#PBS -l walltime= 24:00:00
-#PBS -l select=2:ncpus=128:mpiprocs=128:ompthreads=1:
-#PBS -M abuccheri@physik.hu-berlin.de
-#PBS -j oe
-
-module purge 
-module load intel/2019
-
-EXE=/users/sol/abuccheri/exciting/bin/excitingmpismp
-OUT=terminal.out
-cd $PBS_O_WORKDIR 
-export OMP_NUM_THREADS=1
-
-mpirun -np 256 $EXE > $OUT 2>&1
-    """
-
-    directives = set_pbs_pro_directives([24,0,0],
-                                        'ADD ME',
-                                         nodes=2,
-                                         mpi_ranks_per_node=128,
-                                         omp_threads_per_process=1,
-                                         job_name='HAWK Rome Pure MPI')
-
-    env_vars = OrderedDict([('EXE', '/users/sol/abuccheri/exciting/bin/excitingmpismp'),
+    env_vars = OrderedDict([('EXE', '/zhome/academic/HLRS/pri/ipralbuc/exciting-oxygen_release/bin/exciting_mpismp'),
                             ('OUT', 'terminal.out')
                             ])
-    module_envs = ['intel/2019']
+    module_envs = ['intel/19.1.0', 'mkl/19.1.0', 'impi/19.1.0']
 
-    # TODO Think about how to add this
-    mpi_args = OrderedDict([('pinning', 'omplace')])
+    mpi_options = ['omplace -nt ' + str(omp)]
 
-    script = set_pbs_pro(directives, env_vars, module_envs)
+    script = set_pbs_pro(directives, env_vars, module_envs, mpi_options)
     print(script)
     # assert script == ref
 
 # TODO Write me
-def test_pbs_pro_with_all_args():
+# def test_pbs_pro_with_all_args():
