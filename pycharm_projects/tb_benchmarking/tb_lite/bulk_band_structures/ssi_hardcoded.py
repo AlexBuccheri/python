a27a4a6b7e21825dc9086017d216768cf3200fd2
Alex Buccheri <alexanderbuccheri@googlemail.com>
2022-02-23 17:20:17 +0000

    Moved over tabulated values and deleted from a python file

--- pycharm_projects/tb_benchmarking/tb_lite/bulk_band_structures/ssi_hardcoded.py
+++ pycharm_projects/tb_benchmarking/tb_lite/bulk_band_structures/ssi_hardcoded.py
@@ -224,6 +224,34 @@ ParserOptions {
 #
 # print(material)
 
+# Some previously-tabulated convergence:
+# silicon = [
+#     {'k_grid': [4, 4, 4], 'total_energy': -100.1538},
+#     {'k_grid': [6, 6, 6], 'total_energy': -100.1547},     # Converged
+#     {'k_grid': [8, 8, 8],    'total_energy': -100.1547},  # Use
+#     {'k_grid': [10, 10, 10], 'total_energy': -100.1547}
+# ]
+#
+# ge = [
+#     {'k_grid': [4, 4, 4], 'total_energy': -92.6272},
+#     {'k_grid': [6, 6, 6], 'total_energy': -92.6334},
+#     {'k_grid': [8, 8, 8],    'total_energy': -92.6338},
+#     {'k_grid': [10, 10, 10], 'total_energy': -92.6339},  # Converged
+#     {'k_grid': [12, 12, 12], 'total_energy': -92.6339}   # Use
+# ]
+#
+# diamond = [
+#     {'k_grid': [4, 4, 4], 'total_energy': -116.2287},
+#     {'k_grid': [6, 6, 6], 'total_energy': -116.2284},  # Converged
+#     {'k_grid': [8, 8, 8], 'total_energy': -116.2284}   # Use
+# ]
+#
+# # anatase = [
+# #     {'k_grid': [6, 6, 6], 'total_energy': },
+# #     {'k_grid': [8, 8, 8], 'total_energy': },
+# #     {'k_grid': [10, 10, 10], 'total_energy': },
+# #     {'k_grid': [12, 12, 12], 'total_energy': }
+# # ]
 
 
 
